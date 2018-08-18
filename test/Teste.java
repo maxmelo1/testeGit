550296c1dae6e2bae4daa9cba5b5480cc4e08bfb
teste <teste@teste.com>
2018-08-17 23:57:22 -0300

    Quem Vai no Churras Amanha?????

---
+++ test/Teste.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import testegit.TesteGit;
+
+/**
+ *
+ * @author IFMS
+ */
+public class Teste {
+    
+    public Teste() {
+        
+        
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    
+    public void testa(){
+    
+    testegit.TesteGit teste = new TesteGit();
+    
+        assertEquals(1110, teste.soma(999, 111));
+    }
+}
