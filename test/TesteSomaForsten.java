a67efc0f74fe4e2a8115d7ffbc9533c686f2656f
Bravo <Bravo@DESKTOP-PJRII2R.mshome.net>
2018-08-17 20:00:29 -0400

    Teste Forsten

---
+++ test/TesteSomaForsten.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import testegit.TesteGit;
+
+/**
+ *
+ * @author Bravo
+ */
+public class TesteSomaForsten {
+    
+    public TesteSomaForsten() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    @Test
+     public void somaTeste() {
+     
+         testegit.TesteGit t = new TesteGit();
+         
+         assertEquals(1100, t.soma(1000, 10000));
+         
+     }
+    
+}
